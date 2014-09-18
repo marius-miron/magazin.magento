7b613c542615ab2062aab78368caec616163c3d0
Marius Miron <marius.costel.miron@gmail.com>
2014-09-18 12:33:21 +0300

    added info block for home page functionality

---
+++ app/code/local/Marius/Info/Block/Calculator.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of Calculator
+ *
+ * @author marius
+ */
+class Marius_Info_Block_Calculator extends Mage_Core_Block_Template{
+    //put your code here
+}
---
+++ app/code/local/Marius/Info/Block/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of Task
+ *
+ * @author marius
+ */
+class Marius_Info_Block_Task extends Mage_Core_Block_Template {
+    //put your code here
+}
